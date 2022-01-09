0bd1941d2d7efc97eb8a2395e0af76a519873868
forn <belan.artem2012@gmail.com>
2022-01-09 21:36:09 +0200

    add new app urls 'customer_info'

--- mini_CRM/urls.py
+++ mini_CRM/urls.py
@@ -14,8 +14,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import RedirectView
+
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import IndexView
 
 urlpatterns = [
+    path('', IndexView.as_view(), name='home'),
     path('admin/', admin.site.urls),
+    path('customer_info/', include(('customer_info.urls', 'customer_info'))),
+
+    # path('', RedirectView.as_view(url='/customer_info/', permanent=True)),
 ]
+
+# Add Django site authentication urls (for login, logout, password management)
+# urlpatterns += [
+#     path('accounts/', include('django.contrib.auth.urls')),
+# ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
