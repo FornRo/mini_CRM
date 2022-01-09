ceedfcac7d9a88f061ca4f2f11b9a142cff3108a
forn <belan.artem2012@gmail.com>
2022-01-09 21:38:20 +0200

    add new app 'customer_info' & outside lib: 'crispy_bootstrap5', 'ckeditor'; and added the required components

--- mini_CRM/settings.py
+++ mini_CRM/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # from of outside lib
+    'crispy_forms',
+    'crispy_bootstrap5',
+    'ckeditor',
+    # my app
+    'customer_info',
 ]
 
 MIDDLEWARE = [
@@ -116,9 +122,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+MEDIA_URL = '/media/'
+# MEDIA_ROOT = BASE_DIR / 'media'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
