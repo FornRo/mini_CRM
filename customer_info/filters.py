88f12aa41b36a07c8caa0be2fd5c0fc494bc9c3a
forn <belan.artem2012@gmail.com>
2022-01-12 16:47:48 +0200

    created filters.py

---
+++ customer_info/filters.py
@@ -0,0 +1,24 @@
+from django_filters import FilterSet, ChoiceFilter
+from .models import Company
+
+__all__ = [
+    'CompanyFilter',
+]
+
+
+class CompanyFilter(FilterSet):
+    CHOICES = (
+        ('ascending', 'Ascending'),
+        ('descending', 'Descending'),
+    )
+    name_company = ChoiceFilter(label='Name_company', choices=CHOICES, method='filter_by_order')
+    name = ChoiceFilter(label='name', choices=CHOICES, method='filter_by_order')
+
+    class Meta:
+        model = Company
+        fields = {
+        }
+
+    def filter_by_order(self, qs, name, val):
+        expression = f'{name}' if val == 'True' else f'-{name}'
+        return qs.order_by(expression)
