9d2fb843dfe90fc45378ab6a3aea8235aec1c9e5
forn <belan.artem2012@gmail.com>
2022-01-16 08:22:29 +0200

    change urls.py

--- cooperation/urls.py
+++ cooperation/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', CooperationListView.as_view(), name='list'),
     path('company/<int:pk>', CooperationByCompanyListView.as_view(), name='list_company'),
     path('project/<int:pk>', CooperationByProjectListView.as_view(), name='list_project'),
+    path('user/<int:pk>', CooperationByUserListView.as_view(), name='list_user'),
     path('<int:pk>', CooperationDetailView.as_view(), name='detail'),
 
     path(r'create/', CooperationCreateView.as_view(), name='create'),
--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 from .views import *
 
 
 urlpatterns = [
-    path('', ProjectListView.as_view(), name='list'),
-    path('<int:pk>', ProjectDetailView.as_view(), name='detail'),
-    path(r'create/', ProjectCreateView.as_view(), name='create'),
-    path(r'<int:pk>/update/', ProjectUpdateView.as_view(), name='update'),
-    path(r'<int:pk>/delete/', ProjectDeleteView.as_view(), name='delete'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('managers/', UserListView.as_view(), name='list'),
+    path('register/', RegisterCreateView.as_view(), name='register'),
+    path('profile/', profile, name='profile'),
+    # path('profile/', ProfileFormView.as_view(), name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+
+    path("password_reset", password_reset_request, name="password_reset")
+]
